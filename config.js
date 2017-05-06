15c6e6bc057e75765062c936b04aae1ef151d8c4
Allan Christian Carlos <allanchristiancarlos@gmail.com>
2017-05-06 08:39:27 +0800

    change config to use local node_modules

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ System.config({
     emitDecoratorMetadata: true
   },
   paths: {
-    'npm:': 'https://unpkg.com/'
+    'npm:': 'node_modules/'
   },
   //map tells the System loader where to look for things
   map: {
@@ -21,7 +21,7 @@ System.config({
     '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
     '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
     '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-    '@angular/material': 'npm:@angular/material@2.0.0-beta.3/bundles/material.umd.js',
+    '@angular/material': 'npm:@angular/material/bundles/material.umd.js',
     '@angular/animations': 'npm:@angular/animations/bundles/animations.umd.js',
     '@angular/platform-browser/animations': 'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js',
     '@angular/animations/browser': 'npm:@angular/animations/bundles/animations-browser.umd.js',
@@ -34,10 +34,10 @@ System.config({
     '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
     '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
     
-    'ng2-highcharts': 'npm:ng2-highcharts@1.0.0/bundles/ng2-highcharts.umd.js',
+    'ng2-highcharts': 'npm:ng2-highcharts/bundles/ng2-highcharts.umd.js',
     
     'rxjs': 'npm:rxjs',
-    'typescript': 'npm:typescript@2.2.1/lib/typescript.js'
+    'typescript': 'npm:typescript/lib/typescript.js'
   },
   //packages defines our app package
   packages: {
