4a852691b2b9cac432bc4eb0541d994036f2a2d4
Allan Christian Carlos <allanchristiancarlos@gmail.com>
2017-05-07 10:03:00 +0800

    sort widget types by it's name

--- src/components/widget-manager/widget-manager-widget-types.service.ts
+++ src/components/widget-manager/widget-manager-widget-types.service.ts
@@ -7,7 +7,21 @@ export class WidgetManagerWidgetTypesService {
         WIDGET_MANAGER_WIDGET_TYPES.push(data);
     }
 
+    /**
+     * Return and sort widget types by name
+     */
     getAll() : IWidgetTypeData[] {
-        return WIDGET_MANAGER_WIDGET_TYPES;
+        return WIDGET_MANAGER_WIDGET_TYPES.sort((data1, data2) => {
+            let name1 = data1.name.toUpperCase();
+            let name2 = data2.name.toUpperCase();
+            if (name1 < name2) {
+                return -1;
+            }
+            if (name1 > name2) {
+                return 1;
+            }
+
+            return 0;
+        });
     }
 }
--- src/components/widget/add-widget-dialog.component.ts
+++ src/components/widget/add-widget-dialog.component.ts
@@ -1,9 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { WidgetManagerService } from "../widget-manager/widget-manager.service";
+import { IWidgetTypeData } from "../widget-manager/i-widget-type-data";
 
 @Component({
     selector: 'widget-add-widget-dialog',
     template: `
-        Halo!
+        <ul>
+            <li *ngFor="let widgetType of widgetTypes">{{widgetType.name}}</li>
+        </ul>
     `
 })
-export class AddWidgetDialogComponent {}
+export class AddWidgetDialogComponent implements OnInit {
+    widgetTypes: IWidgetTypeData[];
+
+    constructor(
+        private _widgetManagerSerivce: WidgetManagerService
+    ) {
+        
+    }
+
+    ngOnInit() {
+        this.widgetTypes = this._widgetManagerSerivce.getWidgetTypes();
+    }
+}
